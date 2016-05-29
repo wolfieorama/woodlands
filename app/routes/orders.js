cbe84649285e366cefa1a3c81ef7533743d4efc3
wolfieorama <wachirawamunyi@gmail.com>
2016-05-29 23:50:59 +0300

    updated services with data and generated models

--- app/routes/orders.js
+++ app/routes/orders.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    return [
-      {id: 1, name: 'Nate'},
-      {id: 2, name: 'John'}
-  ];
-  }
+    const id = params.order_id;
+    const store = this.get('store');
+    return store.getOrderById(id);
+  },
+  store: Ember.inject.service() //the local name of the service and the name of the service to inject
+  //beacuse the service name matches the property name we can leave out the 'store'
 });
--- app/services/store.js
+++ app/services/store.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+
+  getOrderById(id){
+    const orders = this.getOrders();
+    return orders;
+  },
+  getOrders(){
+    return [
+      {id: 1, name: 'Nate'},
+      {id: 2, name: 'John'}
+  ];
+}
 });
